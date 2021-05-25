6af1e74a1f10c684568305eb6d295fac118116cd
stevensam0109 <steven.sam.0109@outlook.com>
2021-05-25 09:38:51 +0100

    Ana sayfa hazırlandı

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-linl to="/"><img src="./assets/logo.png" alt="logo"></router-linl>
+    <router-link to="/products">Products</router-link>
+    <router-link to="/card">Card</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -24,5 +22,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
